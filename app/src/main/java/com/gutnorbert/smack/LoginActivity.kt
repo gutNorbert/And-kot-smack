42fd8d602d15ebca329d50bf2a692c00d87c1777
gutNorbert <g.norby21@gmail.com>
2018-12-12 20:03:22 +0100

    Create User Activity

--- app/src/main/java/com/gutnorbert/smack/LoginActivity.kt
+++ app/src/main/java/com/gutnorbert/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.gutnorbert.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
 
 
     fun loginCreateUserBtnClicked(view: View){
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
